534bd5e293106b1279d4cfe7fdcb80c6feac5517
Alexander Alexandrov <aalexandrovv@gmail.com>
2019-09-22 12:37:11 +0300

    Add basic API

--- api.sh
+++ api.sh
@@ -1,5 +1,43 @@
 #!/bin/bash
 
-echo "Home for the current user is: $HOME"
+function create_index() {
+  local index=$1
+  local data=$2
+  curl -H "Accept: application/json" -H "Content-Type: application/json" -XPUT "$ES_URL/$index" -d"$data"
+}
 
-curl -X GET $ES_URL
+function refresh() {
+  local index=$1
+  curl -H "Accept: application/json" -H "Content-Type: application/json" -XPOST "$ES_URL/$index/_refresh"
+}
+
+function get_doc() {
+  local index=$1
+  local id=$2
+  curl -H "Accept: application/json" -H "Content-Type: application/json" -XGET "$ES_URL/$index/_doc/$id"
+}
+
+function create_doc() {
+  local index=$1
+  local data=$2
+  curl -H "Accept: application/json" -H "Content-Type: application/json" -XPOST "$ES_URL/$index/_doc" -d"$data"
+}
+
+function update_doc() {
+  local index=$1
+  local id=$2
+  local data=$3
+  curl -H "Accept: application/json" -H "Content-Type: application/json" -XPUT "$ES_URL/$index/_doc/$id" -d"$data"
+}
+
+function delete_doc() {
+  local index=$1
+  local id=$2
+  curl -H "Accept: application/json" -H "Content-Type: application/json" -XDELETE "$ES_URL/$index/_doc/$id"
+}
+
+function search() {
+  local index=$1
+  local query=$2
+  curl -H "Accept: application/json" -H "Content-Type: application/json" -XPOST "$ES_URL/$index/_search" -d"$query"
+}
--- api.test.sh
+++ api.test.sh
@@ -1,3 +1,50 @@
 #!/bin/bash
 
-. ./api.sh
+source ./api.sh
+
+printf '\n1. Create blog\n'
+
+create_index 'blog' '
+{
+  "mappings": {
+    "properties": {
+      "content": {"type": "text"},
+      "published_at": {
+        "type": "date",
+        "format": "strict_date_optional_time||epoch_millis"
+      },
+      "tags": {"type": "text"},
+      "title": {"type": "text"}
+    }
+  }
+}'
+
+printf '\n2. Post to blog\n'
+
+create_doc 'blog' '
+{
+  "title": "Веселые котята",
+  "content": "<p>Смешная история про котят<p>",
+  "tags": ["котята", "смешная история"],
+  "published_at": "2014-09-12T20:44:42+00:00"
+}'
+
+printf '\n3. Read from blog\n'
+
+get_doc 'blog' '1'
+
+printf '\n4. Refresh index\n'
+
+refresh 'blog'
+
+printf '\n5. Search by tags\n'
+
+search 'blog' '
+{
+  "_source": ["title", "tags"],
+  "filter": {
+    "term": {
+      "tags": "котята"
+    }
+  }
+}'
